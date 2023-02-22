4744b9615f1a346a169799d982eb29e97ba98537
Huawey Trigger <trigger@trigger.com>
2023-02-22 15:04:02 +0300

    [ UPD ] => update header

--- not_cactus/main/views.py
+++ not_cactus/main/views.py
@@ -14,8 +14,10 @@ def show_landing(req):
 
 def show_list_products(req, pk):
     data = {
+        'categories_contact': Categories.objects.order_by('name'),
         'categories': Categories.objects.filter(id=pk),
-        'products': Product.objects.order_by('name')
+        'products': Product.objects.order_by('name'),
+        'categories_blog_contact': CategoriesBlog.objects.all(),
 
     }
     return render(req, 'main/list_products.html', data)
@@ -23,17 +25,22 @@ def show_list_products(req, pk):
 
 def show_products(req, pk):
     data = {
+        'categories_contact': Categories.objects.order_by('name'),
         'products': Product.objects.filter(id=pk),
-        'popular_product': Product.objects.order_by('name')
+        'popular_product': Product.objects.order_by('name'),
+        'categories_blog_contact': CategoriesBlog.objects.all(),
     }
     return render(req, 'main/products.html', data)
 
 
 def show_list_blog(req, pk):
     data = {
+        'categories_contact': Categories.objects.order_by('name'),
+        'categories_blog_contact': CategoriesBlog.objects.all(),
         'categories': CategoriesBlog.objects.filter(id=pk),
         'blogs': Blog.objects.all(),
-        'popular_blog': Blog.objects.all()
+        'popular_blog': Blog.objects.all(),
+        'products_contact': Product.objects.order_by('name'),
     }
 
     return render(req, 'main/list_blog.html', data)
@@ -42,7 +49,10 @@ def show_list_blog(req, pk):
 def show_blog(req, pk):
     data = {
         'blogs': Blog.objects.filter(id=pk),
-        'popular_blog': Blog.objects.all()
+        'popular_blog': Blog.objects.all(),
+        'categories_contact': Categories.objects.order_by('name'),
+        'product_contact': Product.objects.order_by('name'),
+        'categories_blog_contact': CategoriesBlog.objects.all(),
     }
 
     return render(req, 'main/blog.html', data)
